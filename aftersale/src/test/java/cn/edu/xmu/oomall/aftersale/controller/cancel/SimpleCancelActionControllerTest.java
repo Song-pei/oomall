2514ef083b8045c568e935b8fa27def86fa9d711
ligenze <Li7501694032@outlook.com>
2025-12-23 18:17:16 +0800

    simpleTest

---
+++ aftersale/src/test/java/cn/edu/xmu/oomall/aftersale/controller/cancel/SimpleCancelActionControllerTest.java
@@ -0,0 +1,35 @@
+package cn.edu.xmu.oomall.aftersale.controller.cancel;
+
+import cn.edu.xmu.javaee.core.model.UserToken;
+import cn.edu.xmu.oomall.aftersale.dao.bo.AftersaleOrder;
+import cn.edu.xmu.oomall.aftersale.service.strategy.impl.cancel.SimpleCancelAction;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class SimpleCancelActionControllerTest {
+    @InjectMocks
+    private SimpleCancelAction simpleCancelAction;
+
+    @Test
+    public void testExecute() {
+        // 1. 模拟依赖
+        AftersaleOrder mockAftersaleOrder = Mockito.mock(AftersaleOrder.class);
+        // 预设 getId 的返回值（避免 NPE）
+        Mockito.when(mockAftersaleOrder.getId()).thenReturn(123L);
+
+        SimpleCancelAction action = new SimpleCancelAction();
+        UserToken userToken = new UserToken();
+        action.execute(mockAftersaleOrder,userToken);
+
+        // 指定调用次数为 2 次
+        Mockito.verify(mockAftersaleOrder, Mockito.times(2)).getId();
+
+    }
+}
