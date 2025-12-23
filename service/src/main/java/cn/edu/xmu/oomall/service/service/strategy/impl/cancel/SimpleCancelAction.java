00e74f53cafd1210b05ae1983ef8fb6ab96d9d9b
Hyaotao <3062447071@qq.com>
2025-12-23 11:17:32 +0800

    修改了简单取消action

--- service/src/main/java/cn/edu/xmu/oomall/service/service/strategy/impl/cancel/SimpleCancelAction.java
+++ service/src/main/java/cn/edu/xmu/oomall/service/service/strategy/impl/cancel/SimpleCancelAction.java
@@ -3,7 +3,6 @@ package cn.edu.xmu.oomall.service.service.strategy.impl.cancel;
 import cn.edu.xmu.javaee.core.model.UserToken;
 import cn.edu.xmu.oomall.service.dao.bo.ServiceOrder;
 import cn.edu.xmu.oomall.service.service.strategy.action.CancelAction;
-import cn.edu.xmu.oomall.service.dao.bo.ServiceOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -14,16 +13,5 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component("simpleCancelAction")
 public class SimpleCancelAction implements CancelAction {
-
-
-    @Override
-    public Byte execute(ServiceOrder serviceOrder, UserToken user) {
-        log.info("[SimpleCancelAction] 命中简单取消策略，boId={}", serviceOrder.getId());
-        // TODO: 1. 修改数据库状态为 4 (已取消)
-        // TODO: 2. 记录操作日志
-
-
-        //取消状态变更为 已取消(4)
-        return ServiceOrder.CANCEL;
-    }
+    public Byte execute(ServiceOrder serviceOrder, UserToken user) {return ServiceOrder.CANCEL;}
 }
